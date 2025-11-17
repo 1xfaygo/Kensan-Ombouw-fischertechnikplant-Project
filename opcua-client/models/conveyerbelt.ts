fe3f21214a11078923305dcadb95eabedd2421d2
Daniel Bos <89781402+DBosDavinci@users.noreply.github.com>
2025-11-17 10:22:18 +0100

    Deleted unused methods from conveyerbelt

--- opcua-client/models/conveyerbelt.ts
+++ opcua-client/models/conveyerbelt.ts
@@ -5,17 +5,4 @@ export class Conveyerbelt extends BaseModel {
     constructor(session: ClientSession, ns: number) {
         super(session, ns, "Conveyerbelt");
     }
-
-    async checkColor() {
-        return this.read(`Color`);
-    }
-
-    async moveToPosition(position: number) {
-        await this.write("Position", position, DataType.Int16);
-    }
-
-    async pushItem(piston: number) {
-        await this.write(`Piston${piston}`, true, DataType.Boolean);
-        await this.write(`Piston${piston}`, false, DataType.Boolean);
-    }
 }
