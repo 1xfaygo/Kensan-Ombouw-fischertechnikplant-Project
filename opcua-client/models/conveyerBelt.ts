bd245cf490cfd6db2afb80e64dfa14d0bf873195
Hamzq <143701275+HamzaDavinci@users.noreply.github.com>
2025-11-19 19:23:36 +0100

    Delete opcua-client/models/conveyerBelt.ts

--- opcua-client/models/conveyerBelt.ts
+++
@@ -1,8 +0,0 @@
-import { DataType, ClientSession } from "node-opcua";
-import { BaseModel } from "./base";
-
-export class Conveyerbelt extends BaseModel {
-    constructor(session: ClientSession, ns: number) {
-        super(session, ns, "Conveyerbelt");
-    }
-}
