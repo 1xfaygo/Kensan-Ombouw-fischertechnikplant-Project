30457ba56030eaecd41d9eb79f439881882cd2ba
HamzaDavinci <99075941@mydavinci.nl>
2025-11-23 20:56:14 +0100

    Accidental removed items.
    Some items where missing like the vite config leading the site not to start.

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd /d "%~dp0frontend\Kensan"
-start cmd /k "npm run dev"
+start cmd /k "npm run dev --host"
 
 cd /d "%~dp0backend"
-start cmd /k "npm run dev"
+start cmd /k "npm run dev --host"
