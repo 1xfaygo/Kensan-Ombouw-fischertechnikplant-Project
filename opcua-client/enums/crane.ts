bd7243e7797fc6931753643f4d69ea8530057972
Daniel Bos <89781402+DBosDavinci@users.noreply.github.com>
2025-12-02 10:24:56 +0100

    enums

---
+++ opcua-client/enums/crane.ts
@@ -0,0 +1,10 @@
+export enum CraneStatus {
+    idle = 0, // Kraan is idle en klaar voor opdracht
+    moving_1 = 1, // Kraan beweegt naar eerste punt
+    grabbing = 2, // Kraan is op eerste punt en pakt blokje op
+    moving_2 = 3, // Kraan beweegt van eerste naar tweede punt
+    depositing = 4, // Kraan legt blokje neer
+    moving_3 = 5, // Kraan beweegt naar veilige positie.
+    startup = 100, // Kraan is opgestart
+    calibration = 200 // Kraan is gekalibreerd 
+}
